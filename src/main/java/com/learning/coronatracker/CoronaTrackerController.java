c7ca8e015cd8ac3c6410d6dc456342986f8ebff8
sathwik <krishna.chintalapudi@klm.com>
2020-03-07 08:53:52 +0100

    Fixed bugs to handle records with no data - master

--- src/main/java/com/learning/coronatracker/CoronaTrackerController.java
+++ src/main/java/com/learning/coronatracker/CoronaTrackerController.java
@@ -3,6 +3,7 @@ package com.learning.coronatracker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -16,10 +17,10 @@ public class CoronaTrackerController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("locationStats", coronaTrackerService.getCoronaDetails());
-        Integer totalNewCases = coronaTrackerService.getCoronaDetails().stream().mapToInt(s -> s.getVariation()).sum();
-        Integer tatalCases = coronaTrackerService.getCoronaDetails().stream().mapToInt(s -> Integer.parseInt(s.getConfirmedCases())).sum();
+        Integer totalNewCases = coronaTrackerService.getCoronaDetails().stream().filter(s -> s.getVariation() != null).mapToInt(s -> s.getVariation()).sum();
+        Integer totalCases = coronaTrackerService.getCoronaDetails().stream().filter(s -> !StringUtils.isEmpty(s.getConfirmedCases())).mapToInt(s -> Integer.parseInt(s.getConfirmedCases())).sum();
         model.addAttribute("TotalNewCases", totalNewCases);
-        model.addAttribute("TotalCases", tatalCases);
+        model.addAttribute("TotalCases", totalCases);
         return "home";
     }
 
--- src/main/java/com/learning/coronatracker/CoronaTrackerService.java
+++ src/main/java/com/learning/coronatracker/CoronaTrackerService.java
@@ -5,6 +5,7 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
 import java.io.StringReader;
@@ -13,34 +14,38 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class CoronaTrackerService {
-
-    // private static final String CORONA_STAT_FEEDER = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/02-29-2020.csv";
     private static final String CORONA_STAT_FEEDER = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private final static Logger LOGGER = Logger.getLogger(CoronaTrackerService.class.getName());
+
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public List<RegionDetails> getCoronaDetails() {
+
         List<RegionDetails> consolidatedStats = new ArrayList<>();
         try {
+
             HttpClient httpClient = HttpClient.newHttpClient();
             HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(CORONA_STAT_FEEDER)).build();
             HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
             StringReader stringReader = new StringReader(httpResponse.body());
-            List<CSVRecord> data = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(stringReader).getRecords();
-            for (CSVRecord record : data) {
+            List<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(stringReader).getRecords();
+            records.stream().forEach(record -> {
                 RegionDetails regionDetails = new RegionDetails();
                 regionDetails.setState(record.get("Province/State"));
                 regionDetails.setCountry(record.get("Country/Region"));
-                regionDetails.setConfirmedCases(record.get(record.size()-1));
-                regionDetails.setVariation(((Integer.parseInt(record.get(record.size()-1)) - Integer.parseInt(record.get(record.size()-2)))));
+                regionDetails.setConfirmedCases(record.get(record.size() - 1));
+                if (!StringUtils.isEmpty(record.get(record.size() - 1)) && !StringUtils.isEmpty(record.get(record.size() - 2))) {
+                    regionDetails.setVariation(((Integer.parseInt(record.get(record.size() - 1)) - Integer.parseInt(record.get(record.size() - 2)))));
+                }
                 consolidatedStats.add(regionDetails);
-            }
+            });
         } catch (Exception e) {
-            System.out.println("Exception occurred while consolidating statistics");
+            LOGGER.info("Exception occurred while consolidating statistics");
         }
         return consolidatedStats;
     }
